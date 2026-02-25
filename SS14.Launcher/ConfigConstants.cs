4f6d8c59b5ab8f8d0d6c1b49db2d5fa032ae0689
Wylabb <77673282+Wylabb@users.noreply.github.com>
2026-02-25 06:29:00 +0100

    Add additional URLs to fallback sets
    robust-builds.wylab.md were experiencing problems as they use Cloudflare, added non-Cloudflare mirror as a backup

--- SS14.Launcher/ConfigConstants.cs
+++ SS14.Launcher/ConfigConstants.cs
@@ -42,12 +42,14 @@ public static class ConfigConstants
 
     private static readonly UrlFallbackSet RobustBuildsBaseUrl = new([
         "https://robust-builds.wylab.md/",
+        "https://builds-mirror.wylab.md/",
         "https://robust-builds.cdn.spacestation14.com/",
         "https://robust-builds.fallback.cdn.spacestation14.com/"
     ]);
 
     private static readonly UrlFallbackSet LauncherDataBaseUrl = new([
         "https://launcher-data.wylab.md/",
+        "https://data-mirror.wylab.md/",
         "https://launcher-data.cdn.spacestation14.com/",
         "https://launcher-data.fallback.cdn.spacestation14.com/"
     ]);
